2a92b8d44756048f8e1afd0517f4af48d3ec82d8
Shadow <admin@hongyuvip.com>
2017-07-12 14:21:23 +0800

    Forum Templates

--- assets/ForumTemplatesAsset.php
+++ assets/ForumTemplatesAsset.php
@@ -12,25 +12,17 @@ use yii\web\AssetBundle;
  */
 class ForumTemplatesAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/hongyukeji/ehplus/static/';
+    public $sourcePath = '@vendor/hongyukeji/forum-templates/static/';
     public $css = [
-        'css/bootstrap.min14ed.css?v=3.3.6',
-        'css/font-awesome.min93e3.css?v=4.4.0',
-        'css/animate.min.css',
-        'css/style.min862f.css?v=4.1.0',
+        'css/font-awesome.min.css',
+        'css/bootstrap5152.css?ver=1.0',
+        'css/responsive5152.css?ver=1.0',
+        'css/main5152.css?ver=1.0',
+        'css/site.css',
     ];
     public $js = [
-        'js/jquery.min.js?v=2.1.4',
-        'js/bootstrap.min.js?v=3.3.6',
-        'js/plugins/metisMenu/jquery.metisMenu.js',
-        'js/plugins/slimscroll/jquery.slimscroll.min.js',
-        'js/plugins/layer/layer.min.js',
-        'js/hplus.min.js?v=4.1.0',
-        'js/contabs.min.js',
-        'js/plugins/pace/pace.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
